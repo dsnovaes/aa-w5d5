8c7baa13907cedbcda647b405b43b90f8128f3ab
Diego Novaes <dsnovaes1@gmail.com>
2022-11-04 17:10:15 -0700

    still need to fix actor_out_of_work

--- movie_buff/03_queries.rb
+++ movie_buff/03_queries.rb
@@ -1,25 +1,27 @@
 def what_was_that_one_with(those_actors)
   # Find the movies starring all `those_actors` (an array of actor names).
   # Show each movie's title and id.
-  
+  Movie.select(:title, :id).joins(:actors).where("actors.name IN (?)", those_actors).group(:id).having('COUNT(actors.id) >= ?', those_actors.length)
 end
+
+# where('actors.name IN (?)', them)
 
 def golden_age
   # Find the decade with the highest average movie score.
   # HINT: Use a movie's year to derive its decade. Remember that you can use
   # arithmetic expressions in SELECT clauses.
-  
+  Movie.select('AVG(movies.score), (movies.yr/10)*10 AS decade').group('decade').order('AVG(movies.score) DESC').first.decade
 end
 
-def costars(name)
+def costars(target)
   # List the names of the actors that the named actor has ever appeared with.
   # Hint: use a subquery
-  
+  Actor.joins(:movies).where('actors.name != (?) AND movies.id IN (?)', target, Movie.select(:id).joins(:actors).where('actors.name = ?', target)).distinct.pluck(:name)
 end
 
 def actor_out_of_work
   # Find the number of actors in the database who have not appeared in a movie.
-  
+  Actor.select('COUNT(actors.id) AS out_of_work').left_outer_joins(:castings).group('actors.id').where.missing('castings.movie_id').last #.out_of_work
 end
 
 def starring(whazzername)
